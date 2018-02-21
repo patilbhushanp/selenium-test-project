282de217c30c2ed05e6641e5735965a993fe5754
patilbhushanp <patilbhushanp@gmail.com>
2018-02-21 09:59:10 +0530

    Doctor data
    Doctor data

---
+++ src/com/doctor/net/data/KeywordReader.java
@@ -0,0 +1,33 @@
+package com.doctor.net.data;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class KeywordReader {
+
+	public static String KEYWORD_FILE_LOCATION = "../conf/keywords.txt";
+
+	public static List<String> getAllKeywords() {
+		List<String> list = new ArrayList<String>();
+		try {
+			File keywordFile = new File(KEYWORD_FILE_LOCATION);
+			if (keywordFile.isFile()) {
+				FileReader fileReader = new FileReader(keywordFile);
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+				while (true) {
+					String lineData = bufferedReader.readLine();
+					if (lineData == null)
+						break;
+					list.add(lineData);
+				}
+				bufferedReader.close();
+			}
+		} catch (Exception exception) {
+			System.err.println("Failed to get keyword with reason " + exception.getMessage());
+		}
+		return list;
+	}
+}
